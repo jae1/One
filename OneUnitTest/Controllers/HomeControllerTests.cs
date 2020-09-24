bcde0abba8fe6b9d229b64e1e84a8fb94172fe1c
jae1 <jeongjaewon@seattleu.edu>
2020-09-23 17:35:52 -0700

    Adds Unit Tests

---
+++ OneUnitTest/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using One.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace One.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
