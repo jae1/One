d0ef4b1d7d9b23a1a0de73066d5a51dd45bb5c45
jae1 <jeongjaewon@seattleu.edu>
2020-09-30 16:05:50 -0700

    Adds Database

---
+++ One/Data/CoffeeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using One.Models;
+
+
+namespace One.Data
+{
+    public class CoffeeContext : DbContext
+    {
+        public CoffeeContext(DbContextOptions<CoffeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coffee> Coffee { get; set; }
+
+    }
+}
---
+++ One/Migrations/20200930041405_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace One.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coffee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Price = table.Column<float>(nullable: true),
+                    Weight = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    RoastLevel = table.Column<string>(nullable: true),
+                    Taste = table.Column<string>(nullable: true),
+                    Img = table.Column<string>(nullable: true),
+                    CoffeeName = table.Column<string>(nullable: true),
+                    RoastDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coffee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coffee");
+        }
+    }
+}
