987cea5de2462267b5f6cc3bcfae2e5cac84ad56
jae1 <jeongjaewon@seattleu.edu>
2020-09-30 16:06:07 -0700

    Adds coffee model

---
+++ One/Models/Coffee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace One.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
+        public string Image { get; set; }
+        public float? Price { get; set; }
+        public string Weight { get; set; }
+        public string Description { get; set; }
+        public string RoastLevel { get; set; }
+        public string Taste { get; set; }
+        public string Img { get; set; }
+        public string CoffeeName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime RoastDate { get; set; }
+    }
+}
