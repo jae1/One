0cd0f6fe227799d78ec4cea22954d7dc71e1100f
jae1 <jeongjaewon@seattleu.edu>
2020-09-30 16:08:23 -0700

    Updates settings

--- One/Startup.cs
+++ One/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using One.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace One
 {
@@ -24,6 +26,9 @@ namespace One
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<CoffeeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("CoffeeContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
