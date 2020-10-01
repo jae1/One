1022c05e7074d2f119d7fd273fa8d274bf743b97
jae1 <jeongjaewon@seattleu.edu>
2020-10-01 00:06:21 -0700

    Updates Controllers

--- One/Controllers/HomeController.cs
+++ One/Controllers/HomeController.cs
@@ -13,13 +13,9 @@ namespace One.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-/*        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
-*/
-        public HomeController()
-        {
         }
 
         public IActionResult Index()
